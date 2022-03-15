87e9badc6f9e4791b4702388df91a274db2d01bf
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-03-15 09:53:22 -0700

    Betty 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,33 +4,30 @@
 
 /**
  * print_to_98 - check the code
- * @n : int 
- *
+ * @n : int
  * Return: Always void
  */
 void print_to_98(int n)
 {
 	int i = 0;
-	
 	int condition = 99;
-	
 	int step = 1;
-	
+
 	if (n > 98)
 	{
 		condition = 97;
 		step = -1;
 	}
-	for(i = n; i != condition ; i += step)
+	for (i = n; i != condition ; i += step)
 	{
 		int length = snprintf(NULL, 0, "%d", i);
 
 		int z = 0;
 
-		char* str = malloc(length + 1);
-		
+		char *str = malloc(length + 1);
+
 		snprintf(str, length + 1, "%d", i);
-		for(z = 0; z < length; z++)
+		for (z = 0; z < length; z++)
 		{
 			_putchar(str[z]);
 		}
@@ -39,5 +36,5 @@ void print_to_98(int n)
 		_putchar(',');
 		_putchar(' ');
 	}
-	_putchar('\n');	
+	_putchar('\n');
 }
