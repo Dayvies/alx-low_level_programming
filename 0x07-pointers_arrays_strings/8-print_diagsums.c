5030f9f48625d17267bf960fcc9e788e559acea6
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-03-28 08:39:47 -0700

    Changes

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,20 +11,17 @@ void print_diagsums(int *a, int size)
 {
 	unsigned int sum = 0;
 	unsigned int sum2 = 0;
-	unsigned int count =0;
-	int i,j;
+	int i, j, size1;
 
-	for (i = 0; i < (size * size) && size > 0; )
+	size1 = (size * size) - 1;
+	for (i = 0; i <= size1 && size > 0; )
 	{
 		sum = sum + a[i];
 		i = i + size + 1;
 	}
-	for (j = size -1; j < (size * size) && size > 0; )
+	for (j = size -1; j < size1 && size > 0; )
 	{
 		sum2 = sum2 + a[j];
-		count = count + 1;
-		if ((int)count == size)
-			break;
 		j = j + size - 1;
 	}
 	printf("%u, %u\n", sum, sum2);
