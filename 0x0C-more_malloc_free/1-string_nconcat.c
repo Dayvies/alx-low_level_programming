da0530bf88a5e75991da54883225acf67e1a9bf8
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-04-07 16:19:24 +0300

    Mandatory without betty

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1 : string 1
+ * @s2 : string 2
+ * @n : unsigned int size of n 
+ * Return: pointer to new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j = 0, k;
+	char *p;
+
+	if (s1 != NULL)
+	{
+		while (s1[i])
+			i++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[j])
+			j++;
+	}
+	if (n > j)
+		n = j;
+	p = malloc(sizeof(char) * (i + n + 1));
+	if (p == NULL)
+		return (NULL);
+	for(k = 0 ; k < i; k++)
+		p[k] = s1[k];
+	for(k = i; k < i + n; k++)
+		p[k] = s2[k-i];
+	p[k] = '\0';
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * array_range - array of integers from min to max
+ * @min - minimum number
+ * @max - maximum number
+ * Return: pointer to int array
+ */
+int *array_range(int min, int max)
+{
+	int i = 0, j;
+	int *p;
+
+	j = max - min + 1;
+	if (min > max)
+		return (NULL);
+	p = malloc(sizeof(int) * j);
+	if (p == NULL)
+		return (NULL);
+	for (i = min; i < max + 1; i++)
+		p[i] = i;
+	return (p);
+}
