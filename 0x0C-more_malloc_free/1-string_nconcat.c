928367434e6e2baf0fba6c17fa752fadc703cb86
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-04-07 16:24:15 +0300

    Mandatory with betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
  * string_nconcat - concatenates two strings
  * @s1 : string 1
  * @s2 : string 2
- * @n : unsigned int size of n 
+ * @n : unsigned int size of n
  * Return: pointer to new string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -27,10 +27,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p = malloc(sizeof(char) * (i + n + 1));
 	if (p == NULL)
 		return (NULL);
-	for(k = 0 ; k < i; k++)
+	for (k = 0 ; k < i; k++)
 		p[k] = s1[k];
-	for(k = i; k < i + n; k++)
-		p[k] = s2[k-i];
+	for (k = i; k < i + n; k++)
+		p[k] = s2[k - i];
 	p[k] = '\0';
 	return (p);
 }
