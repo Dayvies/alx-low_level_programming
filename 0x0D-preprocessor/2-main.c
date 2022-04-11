e4561ecf3a7ae5c7a172deb8c2f9a4444931fdaa
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-04-11 14:06:43 +0300

    Macro Practice

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,10 @@
 
 /**
  * main - print compiled file name
- * Return : 0 Always
+ * Return: 0 Always
  */
 int main(void)
 {
-	printf("%s\n",__BASE_FILE__);
+	printf("%s\n", __BASE_FILE__);
 	return (0);
 }
