5fa9ff9901ce498592f26e427b4e5bc2e02aa040
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-04-11 19:58:18 +0300

    Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,12 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct dog - dog
+ * @name : name
+ * @age : age
+ * @owner : owner
+ *
+ * Description : Struct for learning how to use structs
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
