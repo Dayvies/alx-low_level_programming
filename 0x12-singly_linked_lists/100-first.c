faa974206a67c327042555438fb5dd18b55d9800
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-04-21 17:48:39 +0300

    premain

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * premain - prints before mainB
+ * Return: void
+ */
+void __attribute__((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
