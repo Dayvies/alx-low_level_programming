d3a6ca230222f87aebf14082d1b296344404dbaa
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-05-03 19:04:47 +0300

    Task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,12 +14,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 	{
-		fd = open(filename, O_WRONLY | O_TRUNC);
-		if (fd < 0)
-			return (-1);
+		return (-1);
 	}
 	while (text_content[buffsize])
 		buffsize++;
