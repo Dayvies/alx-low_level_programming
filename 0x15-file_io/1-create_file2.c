a974b4c1ff2d2c814469b741f5769b38f42fc85f
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-05-03 19:14:40 +0300

    Task 2

---
+++ 0x15-file_io/1-create_file2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * createfile - create file
+ * @filename: filename
+ * @text_content: text_content
+ * Return: 1 if successfull
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t buffsize = 0;
+	long int wr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
+	{
+		return (-1);
+	}
+	while (text_content[buffsize])
+		buffsize++;
+	if (!text_content)
+		return (1);
+	wr = write(fd, text_content, buffsize);
+	close(fd);
+	if (wr != (int)buffsize)
+		return (-1);
+	return (1);
+}
