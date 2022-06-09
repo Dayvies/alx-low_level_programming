083775db3fe50d30be57c49ad0ddae4249fdb96f
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-06-09 12:55:48 +0300

    Upto task 5

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
-	while (current->next != NULL)
+	while (current->next)
 		current = current->next;
 	current->next = new;
 	new->prev = current;
