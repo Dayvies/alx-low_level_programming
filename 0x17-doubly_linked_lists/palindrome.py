60643a8063b503c2be0bf0e77e219b8766bf2fb8
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-06-09 19:26:12 +0300

    palindrome

---
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+x = 0
+palin = []
+
+for i in range(1, 1000):
+    for j in range(1, 1000):
+        palinc = True
+        a = j * i
+        arr = [int(n) for n in str(a)]
+        arrlen = len(arr)
+        ind = arrlen - 1
+        for z in range(int(arrlen/2) + 1):
+            if arr[z] != arr[ind]:
+                palinc = False
+                break
+            ind += -1
+        if palinc == True:
+            palin.append(a)
+print(palin[-5:])
