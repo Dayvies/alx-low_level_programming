959146e16049dfb05f74d45191af66087c9dae9a
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-06-21 20:24:42 +0300

    0 and 1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module creates a class called square. 
+the first instance the class is empty.
+"""
+
+
+class Square:
+    """
+    An empty class that defines a square
+
+    """
+    pass
