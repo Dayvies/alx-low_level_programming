5016ce088ee8adb5fd60478d5384097ae664149d
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-01 16:44:48 +0300

    Setting

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,65 @@
+#include "hash_tables.h"
+
+hash_node_t *create_t(const char *key, const char *value);
+/**
+ * hash_table_set - inserting value
+ * @ht : hash table
+ * @key : Key
+ * @value : value
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *current, *node;
+
+	if ((key == NULL) || (strlen(key) < 1) || value == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+	{
+		current = create_t(key, value);
+		if (current == NULL)
+			return (0);
+		ht->array[index] = current;
+		return (1);
+	}
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			strcpy(current->value, value);
+			return (1);
+		}
+		current = current->next;
+	}
+	current = ht->array[index];
+	node = create_t(key, value);
+	if (node == NULL)
+		return (0);
+	node->next = current;
+	ht->array[index] = node;
+	return (0);
+}
+/**
+ * create_t - create an item
+ * @key : key
+ * @value : value
+ * Return: Node
+ */
+hash_node_t *create_t(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->key = malloc(strlen(key) + 1);
+	node->value = malloc(strlen(value) + 1);
+	if (node->key == NULL || node->value == NULL)
+		return (NULL);
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	node->next = NULL;
+	return (node);
+}
