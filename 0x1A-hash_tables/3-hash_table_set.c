59e6fdd79d17f2a238955d5505edee38e7e6f5ec
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-01 16:54:37 +0300

    Setting

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *current, *node;
 
-	if ((key == NULL) || (strlen(key) < 1) || value == NULL)
+	if (!ht || !key || !strcmp(key, "") || !value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
@@ -29,7 +29,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			strcpy(current->value, value);
+			free(current->value);
+			current->value = strdup(value);
 			return (1);
 		}
 		current = current->next;
