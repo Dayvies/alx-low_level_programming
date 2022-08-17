851690eb4947a1af84a8d725cab3ef9f9480f2c4
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-08-17 17:15:38 +0300

    mandatory debug

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,16 +16,16 @@ def island_perimeter(grid):
     for j in range(len(grid)):
         for i in range(len(grid[j])):
             y = len(grid) - 1
-            z = len(grid[j]) -1
+            z = len(grid[j]) - 1
             x = grid[j][i]
             if x == 1:
-                    if (j-1) < 0 or grid[j-1][i] == 0:
-                        sum += 1
-                    if (i-1) < 0 or grid[j][i-1] == 0:
-                        sum += 1
-                    if (i+1) > z or grid[j][i+1] == 0:
-                        sum += 1
-                    if (j+1) > y or grid[j+1][i] == 0:
-                        sum += 1
-            
+                if (j-1) < 0 or grid[j-1][i] == 0:
+                    sum += 1
+                if (i-1) < 0 or grid[j][i-1] == 0:
+                    sum += 1
+                if (i+1) > z or grid[j][i+1] == 0:
+                    sum += 1
+                if (j+1) > y or grid[j+1][i] == 0:
+                    sum += 1
+
     return(sum)
